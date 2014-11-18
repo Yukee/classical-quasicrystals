9534f906826cf7ec9f67e916c2f3b697b1c91448
Nicolas Macé <nicolas91mace@gmail.com>
2014-11-18 15:15:50 +0100

    working code using Euler scheme, added video

--- fibonacci_chain.py
+++ fibonacci_chain.py
@@ -5,11 +5,14 @@ Created on Mon Nov 17 16:38:59 2014
 @author: nicolas
 """
 
-import pylab,math,numpy
-Ntime = 1000
-L = 32
-alpha = 0.3
+import math
+import matplotlib.pyplot as plt
+Ntime = 600
+L = 89-1
+alpha = 0.8
 dt = 0.2
+# strenght of the quasiperiodic addition to the stiffness
+delta = 5.
 
 # force from harmonic potential
 def HARMONIC(u,n):
@@ -21,8 +24,7 @@ def FPU(u,n):
 
 # inverse golden ratio
 om = 2./(1.+math.sqrt(5))
-# strenght of the quasiperiodic addition to the stiffness
-delta = 0.5
+
 # force from harmonic potential with Fibonacci stiffness
 def FIBO(u,n):
     fibn = int(om*(n+1)) - int(om*n) # an integer to takes values 0 or 1 occordingly to Fibonacci sequence
@@ -33,8 +35,8 @@ def FT(e,k):
     return sum([e[n]*math.sin(n*k*math.pi/L) for n in range(1,L)])/math.sqrt(L/2.)
     
 #initial condition for the position u and velocity v
-u = [0]+[math.sin(n*math.pi/L) for n in range(1,L)]+[0]
-#u=[0]+[L/2.-abs(L/2.-n) for n in range(1,L)]+[0]
+u = [0]+[math.sin(n*math.pi/L)+0.5*math.sin(2*n*math.pi/L)+0.25*math.sin(3*n*math.pi/L) for n in range(1,L)]+[0]
+#u=[0]+[1.-2./L*abs(L/2.-n) for n in range(1,L)]+[0]
 # x0 = L/4 +4
 # x1 = L+1 - x0
 # v0 = 0.02
@@ -63,36 +65,61 @@ for itime in range(Ntime):
     vt.append(v)
 
 def en(k):
-    pylab.title('The Fibonacci chain chain')
-    pylab.xlabel('time')
-    pylab.ylabel('energy of mode ' + str(k))
+    plt.title('The Fibonacci chain chain')
+    plt.xlabel('time')
+    plt.ylabel('energy of mode ' + str(k))
     time = [dt*itime for itime in range(Ntime)]
     #exact = [L*(L*math.sin(math.pi/(2*L)))**2 for itime in range(Ntime)]
-    #pylab.plot(time,exact,'b-')
-    pylab.plot(time,modes[k],'r-')
-    pylab.axis([0,Ntime*dt,min(modes[k])-0.2,max(modes[k])+0.2])#,75.,82.])
-    pylab.show()
+    #plt.plot(time,exact,'b-')
+    plt.plot(time,modes[k],'r-')
+    plt.axis([0,Ntime*dt,min(modes[k])-0.2,max(modes[k])+0.2])#,75.,82.])
+    plt.show()
 
+r = range(L+1)
 def PLOT(t):
-    pylab.title('The Fibonacci chain at time ' + str(t))
-    pylab.xlabel('position')
-    pylab.ylabel('displacement')
-    pylab.plot(range(L+1),ut[int(t/dt)])
-    pylab.show()
+    plt.title('The Fibonacci chain at time ' + str(t))
+    plt.xlabel('position')
+    plt.ylabel('displacement')
+    plt.axis([0,L+1,-2.,2.])
+    plt.plot(r,ut[int(t/dt)])
+    plt.show()
+
+def record(timeit):
+    plt.title('The Fibonacci chain at time ' + str(round(dt*timeit,2)))
+    plt.xlabel('position')
+    plt.ylabel('displacement')
+    plt.axis([0,L+1,-2.,2.])
+    plt.plot(r,ut[timeit])
+    plt.savefig('data/fibo_realspace_'+str(timeit)+'.png')
+    # clear plot
+    plt.clf()
+
+def record_FPU(timeit):
+    plt.title('The FPU chain at time ' + str(round(dt*timeit,2)))
+    plt.xlabel('position')
+    plt.ylabel('displacement')
+    plt.axis([0,L+1,-2.,2.])
+    plt.plot(r,ut[timeit])
+    plt.savefig('data/FPU_realspace_'+str(int(timeit/3))+'.png')
+    # clear plot
+    plt.clf()
+
+# For video output: mencoder "mf://FPU_realspace_%d.png" -mf fps=12:type=png -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell:vbitrate=7000 -vf scale=800:600 -o FPU_movie.avi
+
 
 #r=range(L+1)
 #for t in range(100,400,100):
-#    pylab.title('The Fibonacci chain at several times')
-#    pylab.xlabel('position')
-#    pylab.ylabel('displacement')
-#    pylab.plot(r,ut[int(t/dt)])
+#    plt.title('The Fibonacci chain at several times')
+#    plt.xlabel('position')
+#    plt.ylabel('displacement')
+#    plt.plot(r,ut[int(t/dt)])
 
 #time = [dt*itime for itime in range(Ntime)]
-#pylab.title('The FPU chain')
-#pylab.xlabel('time')
-#pylab.ylabel('energy of modes 1 to 4')
-#pylab.plot(time,modes[1],'r-')
-#pylab.plot(time,modes[2],'b-')
-#pylab.plot(time,modes[3],'b-')
-#pylab.plot(time,modes[4],'b-')
-#pylab.show()
+#plt.title('The FPU chain')
+#plt.xlabel('time')
+#plt.ylabel('energy of modes 1 to 4')
+#plt.plot(time,modes[1],'r-')
+#plt.plot(time,modes[2],'b-')
+#plt.plot(time,modes[3],'b-')
+#plt.plot(time,modes[4],'b-')
+#plt.show()
